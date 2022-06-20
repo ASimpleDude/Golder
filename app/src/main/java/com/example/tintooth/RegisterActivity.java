fded2260fe64b1a7be8a3d0356d694f6e0b8618d
viethmhe150098 <viethmhe150098@fpt.edu.vn>
2022-06-20 21:38:09 +0700

    Part 4
    Chưa xong vẫn đang lỗi

--- app/src/main/java/com/example/tintooth/RegisterActivity.java
+++ app/src/main/java/com/example/tintooth/RegisterActivity.java
@@ -6,6 +6,8 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -16,15 +18,23 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity  extends AppCompatActivity {
-    private EditText emailedit,passedit;
+    private EditText usernameEdt,passedit,emailEdt;
+    private RadioGroup ratioGroup;
+    private RadioButton radio;
     private Button btnregister;
     private FirebaseAuth mAuth;
     void bindingView(){
-        emailedit =findViewById(R.id.username);
+        usernameEdt =findViewById(R.id.username);
         passedit = findViewById(R.id.password);
         btnregister = findViewById(R.id.btnregister);
+        emailEdt = findViewById(R.id.email);
+        ratioGroup = findViewById(R.id.ratioGroup);
+        int selectId = ratioGroup.getCheckedRadioButtonId();
+        radio = findViewById(selectId);
     }
     void bindingAction(){
         btnregister.setOnClickListener(this:: btnregister);
@@ -43,10 +53,17 @@ public class RegisterActivity  extends AppCompatActivity {
     }
 
     private void register() {
-        String email,pass;
-        email = emailedit.getText().toString();
+
+        String username,pass, email;
+        username = usernameEdt.getText().toString();
         pass = passedit.getText().toString();
-        if(TextUtils.isEmpty(email)){
+        email = emailEdt.getText().toString();
+
+        if(radio.getText()==null){
+            return;
+        }
+
+        if(TextUtils.isEmpty(username)){
             Toast.makeText(this,"Vui long nhap Email!!",Toast.LENGTH_SHORT).show();
             return;
         }
@@ -58,12 +75,20 @@ public class RegisterActivity  extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
+                    String userId = mAuth.getCurrentUser().getUid();
+                    DatabaseReference currentUserDb =
+                            FirebaseDatabase.getInstance().getReference()
+                                    .child("Users")
+                                    .child(radio.getText().toString())
+                                    .child(userId)
+                                    .child("name");
+                    currentUserDb.setValue(username);
+
                     Toast.makeText(getApplicationContext(),"Tao tài khoản thanh cong !!",Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
                     startActivity(intent);
                 }else {
                     Toast.makeText(getApplicationContext(),"Register không thanh cong !!",Toast.LENGTH_SHORT).show();
-
                 }
             }
         });
