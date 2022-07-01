63398ab5fcc33191a3573317b0e6de61e2e9c3f9
QuanNC <90890786+QuanNC@users.noreply.github.com>
2022-07-01 20:57:25 +0700

    2 phần đầu của cái video
    ở đây t đã làm xong 2 phần đầu của video, vì mình bắt đầu làm từ p3 nhưng vì giờ đang phải theo vid nên t code lại, thiết kế có thể khác nhưng id ở view cũng như code java thì giống hệt nha.

--- app/src/main/java/com/example/tintooth/MainActivity.java
+++ app/src/main/java/com/example/tintooth/MainActivity.java
@@ -2,95 +2,23 @@ package com.example.tintooth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import com.example.tintooth.DAO.DbConnect;
-
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.lorentzos.flingswipe.SwipeFlingAdapterView;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    private int i;
-    private TextView welcome;
-    private SharedPreferences sharedPreferences;
-    private SharedPreferences.Editor editor;
-    private String username;
 
-    private FirebaseAuth mAuth;
-
-    private String currentUId;
-
-    private DatabaseReference usersDb;
-
-    private ArrayList<String> al;
-    private ArrayAdapter arrayAdapter;
-    private Card card_data[];
-    ListView listView;
-    List<Card> rowItems;
-
-private void bindingView() {
-    welcome = findViewById(R.id.welcomeEdt);
-    sharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
-    username = sharedPreferences.getString("username", "username not found");
-    welcome.setText("Hello " + username);
-}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bindingView();
-
-
-        usersDb = FirebaseDatabase.getInstance().getReference().child("Users");
-
-        mAuth = FirebaseAuth.getInstance();
-        currentUId = mAuth.getCurrentUser().getUid();
-
-
-
-
-
-
-
-
-
-
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Choose_Login_And_Reg.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
     }
-
-
-
-
-
-
-
-
-    public void logoutUser(View view) {
-        mAuth.signOut();
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
-        return;
-    }
-
 
 }
--- app/src/main/java/com/example/tintooth/RegisterActivity.java
+++ app/src/main/java/com/example/tintooth/RegisterActivity.java
@@ -1,81 +1,14 @@
 package com.example.tintooth;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import android.os.Bundle;
 
-public class RegisterActivity  extends AppCompatActivity {
-    private EditText usernameEdt,passedit,emailEdt;
-
-    private Button btnregister;
-    private FirebaseAuth mAuth;
-    void bindingView(){
-        usernameEdt =findViewById(R.id.username);
-        passedit = findViewById(R.id.password);
-        btnregister = findViewById(R.id.btnregister);
-        emailEdt = findViewById(R.id.email);
-
-    }
-    void bindingAction(){
-        btnregister.setOnClickListener(this:: btnregister);
-    }
-    private void btnregister(View view) {
-        register();
-    }
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        mAuth=FirebaseAuth.getInstance();
-        bindingView();
-        bindingAction();
-    }
-
-    private void register() {
-
-        String username,pass, email;
-        username = usernameEdt.getText().toString();
-        pass = passedit.getText().toString();
-        email = emailEdt.getText().toString();
-
-
-        if(TextUtils.isEmpty(username)){
-            Toast.makeText(this,"Vui long nhap Email!!",Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if(TextUtils.isEmpty(pass)){
-            Toast.makeText(this,"Vui long nhap Email!!",Toast.LENGTH_SHORT).show();
-            return;
-        }
-        mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(getApplicationContext(),"Tao tài khoản thanh cong !!",Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
-                    startActivity(intent);
-                }else {
-                    Toast.makeText(getApplicationContext(),"Register không thanh cong !!",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
     }
 }
