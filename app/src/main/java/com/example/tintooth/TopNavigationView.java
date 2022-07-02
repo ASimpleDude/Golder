57559aeb2faa067861c2f88e960b558a91d2b728
QuanNC <90890786+QuanNC@users.noreply.github.com>
2022-07-02 21:57:35 +0700

    Xong phần 10, 11, 12

--- app/src/main/java/com/example/tintooth/TopNavigationView.java
+++ app/src/main/java/com/example/tintooth/TopNavigationView.java
@@ -12,28 +12,28 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class TopNavigationView {
-    private static final String TAG = "TopNavigationHelper";
+     private static final String TAG = "TopNavigationHelper";
      public static void setupTopNavigationView(BottomNavigationViewEx tv){
         Log.d(TAG, "setupTopNavigation: setting up navigationview");
     }
 
-//    public static void enableNavigation(final Context context, BottomNavigationViewEx view){
-//         view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-//             @Override
-//             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                 switch (item.getItemId()){
-//                     case R.id.profile:
-//                         Intent i = new Intent(context, SettingsActivity.class);
-//                         context.startActivity(i);
-//                         break;
-//                     case R.id.ic_matched:
-//                         Intent intent =new Intent(context, MatchesActivity.class);
-//                         context.startActivity(intent);
-//                         break;
-//
-//                 }
-//                 return false;
-//             }
-//         });
-//    }
+    public static void enableNavigation(final Context context, BottomNavigationViewEx view){
+         view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+             @Override
+             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                 switch (item.getItemId()){
+                     case R.id.ic_profile:
+                         Intent i = new Intent(context, SettingsActivity.class);
+                         context.startActivity(i);
+                         break;
+                     case R.id.ic_matched:
+                         Intent intent =new Intent(context, MatchesActivity.class);
+                         context.startActivity(intent);
+                         break;
+
+                 }
+                 return false;
+             }
+         });
+    }
 }
