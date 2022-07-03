1891105bd7e61adc572a392e1ce45329279c9d15
ASimpleDude <ranhoangpro@gmail.com>
2022-07-03 15:18:52 +0700

    xong phần code setting(part17)
    trong video có activity_setting.xml và toolbar_setting.xml bị trùng id settings_toolbartag nên t xóa 1 chữ "s" ở cái bên toolbar_setting.xml

--- app/src/main/java/com/example/tintooth/SettingsActivity.java
+++ app/src/main/java/com/example/tintooth/SettingsActivity.java
@@ -1,10 +1,25 @@
 package com.example.tintooth;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -13,11 +28,29 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SettingsActivity extends AppCompatActivity {
     private EditText mNameField, mPhoneField, mBudget;
@@ -31,7 +64,7 @@ public class SettingsActivity extends AppCompatActivity {
     private String userId, name, phone, profileImageUrl, userSex, userBudget, userNeed, userGive;
     private int needIndex, giveIndex;
     private Uri resultUri;
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,13 +104,13 @@ public class SettingsActivity extends AppCompatActivity {
 
         give.setAdapter(adapter_give);
 
-        //get User Info();
+        getUserInfo();
         mProfileImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(!checkPermission()){
                     Toast.makeText(SettingsActivity.this, "Allow access to continue!", Toast.LENGTH_SHORT).show();
-                    requestPermissions();
+                    requestPermission();
                 }
                 else{
                     Intent intent = new Intent(Intent.ACTION_PICK);
@@ -86,6 +119,268 @@ public class SettingsActivity extends AppCompatActivity {
                 }
             }
         });
+        mConfirm.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinner.setVisibility(View.VISIBLE);
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                finish();return;
+            }
+        });
+        androidx.appcompat.widget.Toolbar toolbar= findViewById(R.id.settings_toolbartag);
+        setSupportActionBar(toolbar);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+    public boolean checkPermission(){
+        int result= ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+    public void requestPermission(){
+        ActivityCompat.requestPermissions(this,new String[]{READ_EXTERNAL_STORAGE}, 100);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
+            } else {
+                Toast.makeText(this, "Allow access to continue", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.Contact){
+            new AlertDialog.Builder(SettingsActivity.this)
+                    .setTitle("Contact Us")
+                    .setMessage("Contact Us: ABCD")
+                    .setNegativeButton("Dismiss", null)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+        else if(item.getItemId()==R.id.logout){
+            spinner.setVisibility(View.VISIBLE);
+            mAuth.signOut();
+            Toast.makeText(this, "Log out successfull", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(SettingsActivity.this, Choose_Login_And_Reg.class);
+            startActivity(intent);
+            finish();
+            spinner.setVisibility(View.GONE);
+        }
+        else if(item.getItemId()==R.id.deleteAccount){
+            new AlertDialog.Builder(SettingsActivity.this)
+                    .setTitle("Are you sure?")
+                    .setMessage("Delete account will also remove all your data from system")
+                    .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            mAuth.getCurrentUser().delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    spinner.setVisibility(View.VISIBLE);
+                                    if(task.isSuccessful()){
+                                        deleteUserAccount(userId);
+                                        Toast.makeText(SettingsActivity.this, "Account deleted", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(SettingsActivity.this, Choose_Login_And_Reg.class);
+                                        startActivity(intent);
+                                        finish();
+                                        spinner.setVisibility(View.GONE);
+                                        return;
+                                    }
+                                    else{
+                                        Toast.makeText(SettingsActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                        mAuth.signOut();
+                                        Intent intent = new Intent(SettingsActivity.this, Choose_Login_And_Reg.class);
+                                        startActivity(intent);
+                                        finish();
+                                        spinner.setVisibility(View.VISIBLE);
+                                        return;
+                                    }
+                                }
+                            });
+                        }
+                    }).setNegativeButton("Dismiss", null)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        } return super.onOptionsItemSelected(item);
+    }
+    public void deleteMatch(String matchId, String chatId){
+        DatabaseReference matchId_in_UserId_dbReference = FirebaseDatabase.getInstance().getReference().child("Users")
+                .child(userId).child("connections").child("matches").child(matchId);
+        DatabaseReference userId_in_matchId_dbReference = FirebaseDatabase.getInstance().getReference().child("Users")
+                .child(userId).child("connections").child("matches").child(userId);
+        DatabaseReference yeps_in_matchId_dbReference = FirebaseDatabase.getInstance().getReference().child("Users")
+                .child(userId).child("connections").child("yeps").child(userId);
+        DatabaseReference yeps_in_UserId_dbReference = FirebaseDatabase.getInstance().getReference().child("Users")
+                .child(userId).child("connections").child("yeps").child(matchId);
+        DatabaseReference matchId_chat_dbReference = FirebaseDatabase.getInstance().getReference().child("Chat")
+                .child(chatId);
+        matchId_chat_dbReference.removeValue();
+        matchId_in_UserId_dbReference.removeValue();
+        userId_in_matchId_dbReference.removeValue();
+        yeps_in_matchId_dbReference.removeValue();
+        yeps_in_UserId_dbReference.removeValue();
+    }
+    private void deleteUserAccount(String userId) {
+        DatabaseReference curruser_ref = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
+        DatabaseReference curruser_matches_ref = FirebaseDatabase.getInstance().getReference()
+                .child("Users").child(userId).child("connections").child("matches");
+        curruser_matches_ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot match: dataSnapshot.getChildren()){
+                        deleteMatch(match.getKey(), match.child("ChatId").getValue().toString());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        curruser_matches_ref.removeValue();
+        curruser_ref.removeValue();
+    }
+    private void getUserInfo(){
+        mUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()&&dataSnapshot.getChildrenCount()>0){
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                    if(map.get("name")!=null){
+                        name=map.get("name").toString();
+                        mNameField.setText(name);
+                    }
+                    if(map.get("phone")!=null){
+                        name=map.get("phone").toString();
+                        mPhoneField.setText(phone);
+                    }if(map.get("sex")!=null){
+                        userSex=map.get("sex").toString();
+                    }
+                    if(map.get("budget")!=null){
+                        userBudget=map.get("budget").toString();
+                    }else
+                        userBudget="0";
+                    if(map.get("give")!=null){
+                        userGive=map.get("give").toString();
+                    }else userGive="";
+                        if (map.get("need") != null) {
+                            userNeed=map.get("need").toString();
+
+                        }else userNeed="";
+                        String[] services = getResources().getStringArray(R.array.services);
+                        needIndex=giveIndex=0;
+                        for (int i=0;i<services.length;i++){
+                            if(userNeed.equals(services[i])) needIndex=1;
+                            if(userGive.equals(services[i])) giveIndex=1;
+                        }
+                        need.setSelection(needIndex);
+                        give.setSelection(giveIndex);
+                        mBudget.setText(userBudget);
+
+                        Glide.clear(mProfileImage);
+                        if(map.get("profileImageUrl")!=null){
+                            profileImageUrl=map.get("profileImageUrl").toString();
+                            switch (profileImageUrl){
+                                case "default":
+                                    Glide.with(getApplication()).load(R.drawable.profile).into(mProfileImage);
+                                    break;
+                                default:
+                                    Glide.with(getApplication()).load(profileImageUrl).into(mProfileImage);
+                                    break;
+                            }
+                        }
+                    }
+                }
+
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });}
+        private void saveUserInformation(){
+            name= mNameField.getText().toString();
+            phone=mPhoneField.getText().toString();
+            userBudget=mBudget.getText().toString();
+            userGive=give.getSelectedItem().toString();
+            userNeed=need.getSelectedItem().toString();
+
+            Map userInfo = new HashMap();
+            userInfo.put("name", name);
+            userInfo.put("phone", phone);
+            userInfo.put("need", need);
+            userInfo.put("give", give);
+            userInfo.put("budget", userBudget);
+            mUserDatabase.updateChildren(userInfo);
+            if(resultUri!=null){
+                StorageReference filepath= FirebaseStorage.getInstance().getReference().child("profileImages").child(userId);
+                Bitmap bitmap= null;
+
+                try{
+                    bitmap= MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(), resultUri);
+
+                }
+                catch(IOException e){
+                    e.printStackTrace();
+                }
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+                byte[] data = baos.toByteArray();
+                UploadTask uploadTask = filepath.putBytes(data);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        finish();
+                    }
+                });
+                uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Task<Uri> uri = taskSnapshot.getStorage().getDownloadUrl();
+                        while (!uri.isComplete());
+                        Uri downloadUri = uri.getResult();
+                        Map userInfo = new HashMap();
+                        userInfo.put("profileImageUrl", downloadUri.toString());
+                        mUserDatabase.updateChildren(userInfo);
+                        finish();
+                        return;
+                    }
+                });
+            } else{finish();}
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1&& resultCode== Activity.RESULT_OK){
+            final Uri imageUri = data.getData();
+            resultUri=imageUri;
+            mProfileImage.setImageURI(resultUri);
+        }
+    }
 }
